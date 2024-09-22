f6b939c8338fb5fe1935261575fa3bfa5186c9ac
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-09-22 21:40:44 +0800

    Add Automapper profile

---
+++ src/Application/Common/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyMeLoan.Application.Handlers.Customers.Dtos;
+using MoneyMeLoan.Application.Handlers.Loans.Dtos;
+using MoneyMeLoan.Application.Handlers.Products.Dtos;
+using MoneyMeLoan.Domain.Entities;
+
+namespace MoneyMeLoan.Application.Common.Mappings;
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        //Customer
+        CreateMap<Customer, CustomerDto>().ReverseMap();
+
+        //Product
+        CreateMap<Product, ProductDto>().ReverseMap();
+
+        //Loan
+        CreateMap<Loan,LoanDto>().ReverseMap();
+
+    }
+}
