d292a7008f88295c26d04d38561f5bfc7a8bb1bf
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-09-25 22:47:34 +0800

    Confirm loan component

--- src/Web/ClientApp/src/app/loan/loan-confirm/loan-confirm.component.ts
+++ src/Web/ClientApp/src/app/loan/loan-confirm/loan-confirm.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoanDto, LoansClient } from 'src/app/web-api-client';
 
 @Component({
   selector: 'app-loan-confirm',
   templateUrl: './loan-confirm.component.html',
   styleUrls: ['./loan-confirm.component.css']
 })
-export class LoanConfirmComponent {
+export class LoanConfirmComponent implements OnInit {
 
+  loanId: string = "";
+  loanDto: LoanDto;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private loansClient: LoansClient
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.loanId = params['id'];
+      console.log(this.loanId + " loan Id");
+    });
+
+    if (this.loanId != "") {
+      this.loansClient.getLoanById(this.loanId).subscribe(
+        result => {
+          this.loanDto = result.data;
+          console.log(this.loanDto);
+        },
+        error => console.error(error)
+      );
+    }
+  }
 }
